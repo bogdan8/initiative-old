78b105c0dc41e296a9d2e58b8f5c210f8cea108e
bogdan8 <bogdanlitvin8@gmail.com>
2016-06-29 13:18:31 +0300

    [~] change validate in initiative model and change localization on uk

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,7 +2,8 @@ class AdminsController < ApplicationController
   load_and_authorize_resource class: User, except: [:create]
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to main_app.root_url, alert: exception.message
+    flash[:error] = exception.message
+    redirect_to main_app.root_url
   end
 
   def index
--- app/models/initiative.rb
+++ app/models/initiative.rb
@@ -2,10 +2,22 @@ class Initiative < ActiveRecord::Base
   validates :title, presence: true
   validates :long_description, presence: true
   validates :short_description, length: { maximum: 200 }
-  validates :sum, format: { with: /\A\d{3,5}\b/ }
-  validates :term_fundraiser, format: { with: /\A30\b|\A60\b/ }
-  validates :term_report, format: { with: /\A15\b|\A30\b/ }
+  validate :validate_sum
+  validate :validate_term_fundraiser
+  validate :validate_term_report
 
-  has_attached_file :main_picture, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
-  validates_attachment_content_type :main_picture, content_type: %r{\Aimage\/.*\Z/}
+  has_attached_file :main_picture, styles: {medium: '300x300>', thumb: '100x100>'}, default_url: '/images/:style/missing.png'
+  validates_attachment_content_type :main_picture, content_type: %r{\Aimage\/.*\Z}
+
+  def validate_sum
+    errors.add(:sum, 'Число має бути більше чим 100 і менше чим 50000') unless self.sum >= 100 && self.sum <= 50_000
+  end
+
+  def validate_term_fundraiser
+    errors.add(:term_fundraiser, 'Ви вели невірне число') unless self.term_fundraiser == 30 || self.term_fundraiser == 60
+  end
+
+  def validate_term_report
+    errors.add(:term_report, 'Ви вели невірне число') unless self.term_report == 15 || self.term_report == 30
+  end
 end
