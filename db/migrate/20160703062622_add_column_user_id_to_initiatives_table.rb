ad19a5804ec25cd273bf98be1535bb999ef5bac2
bogdan8 <bogdanlitvin8@gmail.com>
2016-07-03 09:34:58 +0300

    [+] add user_id column to initiatives table

---
+++ db/migrate/20160703062622_add_column_user_id_to_initiatives_table.rb
@@ -0,0 +1,5 @@
+class AddColumnUserIdToInitiativesTable < ActiveRecord::Migration
+  def change
+    add_column :initiatives, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160702090040) do
+ActiveRecord::Schema.define(version: 20160703062622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20160702090040) do
     t.integer  "main_picture_file_size"
     t.datetime "main_picture_updated_at"
     t.string   "aasm_state"
+    t.integer  "user_id"
   end
 
   create_table "roles", force: :cascade do |t|
