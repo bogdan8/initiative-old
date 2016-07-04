60047d79b175ade6aa4df3f8f1b81d91268f662a
bogdan8 <bogdanlitvin8@gmail.com>
2016-07-04 15:56:09 +0300

    [+] add pagination to index page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @initiatives = Initiative.where(aasm_state: :fundraiser).order(created_at: :desc)
+    @initiatives = Initiative.where(aasm_state: :fundraiser).order(created_at: :desc).page(params[:page]).per(1)
   end
 end
