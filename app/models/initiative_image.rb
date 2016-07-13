95fcf87ac028b98a1b3fa9b92e7f4f3a39215b2e
bogdan8 <bogdanlitvin8@gmail.com>
2016-07-13 14:16:40 +0300

    [~] change initiative_images model on initiative_image

--- app/models/initiative_image.rb
+++ app/models/initiative_image.rb
@@ -1,15 +1,15 @@
-class InitiativeCategory < ActiveRecord::Base
+class InitiativeImage < ActiveRecord::Base
   size_initiative_images = { medium: '300x300>', thumb: '100x100>' }
   path_initiative_images = ':rails_root/public/images/:class/:attachment/:id/:style/:filename'
 
-  belongs_to :initiative
+  has_one :initiative
 
-  has_attached_file :main_picture,
+  has_attached_file :initiative_images,
                     styles: size_initiative_images,
                     path: path_initiative_images,
                     url: '/images/:class/:attachment/:id/:style/:filename',
                     default_url: '/images/:style/missing.png'
-  validates_attachment_content_type :main_picture, content_type: %r{\Aimage\/.*\Z}
+  validates_attachment_content_type :initiative_images, content_type: %r{\Aimage\/.*\Z}
 
   scopify
 end
