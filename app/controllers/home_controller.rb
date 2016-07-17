ec7c0b719c7f93a177811175a985d1279f5d79f8
bogdan8 <bogdanlitvin8@gmail.com>
2016-07-17 10:21:55 +0300

    [~] refactoring code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
-
   def index
     @initiatives = Initiative.fundraiser.order(created_at: :desc).page(params[:page]).per(6)
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # devise setting
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost' }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
@@ -17,7 +17,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -41,4 +41,13 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  ActionMailer::Base.smtp_settings = {
+    address:        'smtp.gmail.com',
+    port:           587,
+    authentication: :plain,
+    user_name:      'bogdanlitvin8@gmail.com',
+    password:       '70sunogo',
+    openssl_verify_mode:  'none'
+  }
 end
