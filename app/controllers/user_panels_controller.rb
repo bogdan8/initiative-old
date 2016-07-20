9a0a6508d0f6d25eb71f6cf81a2657dce01ef1be
bogdan8 <bogdanlitvin8@gmail.com>
2016-07-20 12:48:55 +0300

    [+] add ajax pagination to user_panel index

--- app/controllers/user_panels_controller.rb
+++ app/controllers/user_panels_controller.rb
@@ -2,7 +2,10 @@ class UserPanelsController < ApplicationController
   load_and_authorize_resource class: Initiative
 
   def index
-    @initiatives = Initiative.where('user_id = ?', current_user.id) unless current_user.has_role? :admin
+    @initiatives = Initiative.where('user_id = ?',
+                                    current_user.id).page(params[:page]).per(3) unless current_user.has_role? :admin
+    @last_page = @initiatives.last_page?
+    @current_page = @initiatives.current_page
   end
 
   def show_user
