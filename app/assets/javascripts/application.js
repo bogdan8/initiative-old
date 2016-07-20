d1a833ec211661f149b8109cb253720df55dffce
bogdan8 <bogdanlitvin8@gmail.com>
2016-07-20 10:21:42 +0300

    [~] bag fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,8 @@
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require bootstrap
 //= require adminlte
 //= require select2/select2.min.js
 //= require initiatives
 //= require application_layout
+//= require turbolinks
