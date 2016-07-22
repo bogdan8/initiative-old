b6fc739b164ec1c0c0f37dd99a8a63d9b9e93fc0
bogdan8 <bogdanlitvin8@gmail.com>
2016-07-22 10:20:03 +0300

    [+] add spinner css to initiative new and edit

--- app/assets/javascripts/initiatives.js
+++ app/assets/javascripts/initiatives.js
@@ -2,4 +2,8 @@ $(document).ready(function () {
     $(".category_select").select2({
         placeholder: 'Натисніть сюди щоб вибрати категорії'
     });
+    $('form').submit(function () {
+        $('.initiative-button-submit').css('display', 'none');
+        $('.sk-cube-grid').css('display', 'block');
+    });
 });
