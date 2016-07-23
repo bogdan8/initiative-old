e4ef8a2d1fd39a391b10e52b6ddf8c21f8b9bb27
bogdan8 <bogdanlitvin8@gmail.com>
2016-07-23 14:38:12 +0300

    [+] add test for my validation in initiatives

--- spec/models/initiative_spec.rb
+++ spec/models/initiative_spec.rb
@@ -16,11 +16,18 @@ describe Initiative, type: :model do
     it 'is title not be empty' do
       initiative = create(:initiative)
       expect(initiative.title).not_to be_empty
-
     end
     it 'is long_description not be empty' do
       initiative = create(:initiative)
       expect(initiative.long_description).not_to be_empty
+    end
+    it 'is term_fundraiser must be 30 or 60' do
+      initiative = create(:initiative)
+      expect(initiative.term_fundraiser).to eq(30 || 60)
+    end
+    it 'is term_report must be 15 or 30' do
+      initiative = create(:initiative)
+      expect(initiative.term_report).to eq(15 || 30)
     end
   end
 end
