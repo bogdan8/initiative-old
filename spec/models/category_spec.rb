3d8ec08fc53deb4329e84b2db5ee03920928e33d
bogdan8 <bogdanlitvin8@gmail.com>
2016-07-23 16:24:25 +0300

    [+] add test to category model for associations

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Category, type: :model do
+  describe 'test for associations' do
+    it { should have_many(:initiative_category) }
+    it { should have_many(:initiatives).through(:initiative_category) }
+  end
+end
