3f10720a5aaf37ecb0e755979ac327479f1b312b
bogdan8 <bogdanlitvin8@gmail.com>
2016-07-23 15:24:22 +0300

    [+] add test for add initiative

---
+++ spec/features/add_initiative_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+feature 'adding initiatives' do
+  scenario 'allow a user to add a initiative' do
+    initiative = create(:initiative)
+
+    visit initiative_path(initiative)
+
+    expect(page).to have_content('Категорія:')
+  end
+end
--- spec/support/session_helper.rb
+++ spec/support/session_helper.rb
@@ -1,13 +1,3 @@
-def create_an_account
-  visit new_user_registration_path
-
-  fill_in 'user_email', with: 'user@user.com'
-  fill_in 'user_password', with: '123456'
-  fill_in 'user_password_confirmation', with: '123456'
-
-  click_button 'Зареєструватись'
-end
-
 def login
   visit new_user_session_path
 
