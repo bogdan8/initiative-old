5936478f76b4c67854e194988100991b68556399
bogdan8 <bogdanlitvin8@gmail.com>
2016-08-02 16:02:20 +0300

    [+] add style to login social butto

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,12 @@ class User < ActiveRecord::Base
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      unless auth.info.email.nil?
-        user.email = auth.info.email
-      else
+      if auth.info.email.nil?
         user.email = "ifcity#{rand(50_000)}@city.com"
+      else
+        user.email = auth.info.email
       end
-      unless auth.info.image.nil?
-        user.avatar_file_name = auth.info.image
-      end
+      user.avatar_file_name = auth.info.image unless auth.info.image.nil?
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
     end
@@ -27,7 +25,7 @@ class User < ActiveRecord::Base
     UserMailer.send_message_welcome_to_new_user(self).deliver_later
   end
 
-  size_avatar = {medium: '300x300>', thumb: '100x100>'}
+  size_avatar = { medium: '300x300>', thumb: '100x100>' }
   path_avatar = ':rails_root/public/images/:attachment/:id/:style/:filename'
 
   rolify
