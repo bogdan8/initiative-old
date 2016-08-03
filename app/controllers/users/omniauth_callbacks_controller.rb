9a95c4280d1e60447618811047a13be6b69f5d72
bogdan8 <bogdanlitvin8@gmail.com>
2016-08-03 12:35:13 +0300

    [~] bag fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,16 +1,16 @@
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    before_action :social_user, only: [:twitter, :facebook]
+
     def twitter
-      user = User.from_omniauth(env['omniauth.auth'])
-      if user.persisted?
-        sign_in_and_redirect user, notice: 'Ви успішно увійшли!'
-      else
-        redirect_to new_user_registration_url
-        session['devise.user_attributes'] = user.attributes
-      end
     end
 
     def facebook
+    end
+
+    private
+
+    def social_user
       user = User.from_omniauth(env['omniauth.auth'])
       if user.persisted?
         sign_in_and_redirect user, notice: 'Ви успішно увійшли!'
@@ -18,11 +18,6 @@ module Users
         redirect_to new_user_registration_url
         session['devise.user_attributes'] = user.attributes
       end
-    end
-
-    def destroy
-      session[:user_id] = nil
-      redirect_to root_url, notice: 'Ви успішно вийшли!'
     end
   end
 end
